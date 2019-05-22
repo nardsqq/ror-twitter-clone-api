2843cb87ca0cddb44d9f11f848e893236086b4c1
nardsqq <nardparagas@gmail.com>
2019-05-22 20:07:06 +0800

    Add an index method to display tweets

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::TweetsController < ApplicationController
+	# List all existing tweets ordered by date created
+	def index
+		@tweets = Tweet.order(:created_at)
+
+		render json: @tweets, status: 200
+	end
 end
