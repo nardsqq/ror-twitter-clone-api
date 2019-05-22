96e22406be19e7a8aefc03f036c8ebb58b5f466d
nardsqq <nardparagas@gmail.com>
2019-05-22 20:08:39 +0800

    Add a create method to store tweets

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -5,4 +5,13 @@ class Api::V1::TweetsController < ApplicationController
 
 		render json: @tweets, status: 200
 	end
+
+	# Create a tweet resource
+	def create
+		@tweet = Tweet.new(tweet_params)
+
+		@tweet.save
+
+		render json: @tweet, status: 201
+	end
 end
