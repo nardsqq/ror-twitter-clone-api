2ff4a069e4ef4c46d0dd1e73508f60ef9ae242e8
nardsqq <nardparagas@gmail.com>
2019-05-24 12:42:53 +0800

    Replace default Devise routes into custom ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :tweets    
+      resources :tweets
+      resources :sessions, only: [:create, :destroy]    
     end
   end
 end
