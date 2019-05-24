2eddbf148fa70c728026903d38cffde3d022e144
nardsqq <nardparagas@gmail.com>
2019-05-24 15:37:25 +0800

    Add :authentication_token attribute
    Add a unique :authentication_token attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_041437) do
+ActiveRecord::Schema.define(version: 2019_05_24_072240) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tweet"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_05_24_041437) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
