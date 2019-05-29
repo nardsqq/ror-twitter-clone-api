5f1351ec0e136a8b98e235325e98f3be881fd89e
nardsqq <nardparagas@gmail.com>
2019-05-29 12:25:06 +0800

    Sets default URLs for home and profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define root URL for the application
   root 'pages#index'
+  
   # Pages routes
-  get 'pages/home'
-  get 'pages/profile'
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+
   devise_for :users
   # devise_for :users
   namespace :api do
