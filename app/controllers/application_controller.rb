d27a6f291cedfa729f5be9c99eee83adf0386f65
nardsqq <nardparagas@gmail.com>
2019-05-30 12:14:38 +0800

    Add strong parameter permissions during sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prepend protect_from_forgery to the before_action chain manually
   protect_from_forgery prepend: true
+
+  # Call the configured parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Allow selected fields to be updated during mass assignment
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[username
+                                                           email password
+                                                           password_confirmation
+                                                           remember_me])
+      devise_parameter_sanitizer.permit(:sign_in, keys: %i[username
+                                                           email password
+                                                           password_confirmation])
+    end
 end
