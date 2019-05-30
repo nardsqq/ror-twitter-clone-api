9b5a83fa2d1f455fb6f1ecfb3f881ab460d4a33f
nardsqq <nardparagas@gmail.com>
2019-05-30 15:47:10 +0800

    Fixes typo error in dependent option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, length: { maximum: 30 }
-  has_many :tweets, dependent: destroy
 end
