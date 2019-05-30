2279244ed3db19c1ff76f390301e47204693eddd
nardsqq <nardparagas@gmail.com>
2019-05-30 11:36:08 +0800

    Creates a migration that adds username column
    Creates a migration that adds a username column to Devise's users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_035029) do
+ActiveRecord::Schema.define(version: 2019_05_30_033104) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tweet"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 2019_05_29_035029) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", limit: 30
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
