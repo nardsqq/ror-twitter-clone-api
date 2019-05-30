2ff9c116eafb61785970caed8617e91444ccba30
nardsqq <nardparagas@gmail.com>
2019-05-30 15:25:26 +0800

    Adds association to tweet model to user

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :tweet, presence: true
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :text, presence: true, length: { maximum: 140 }
 end
--- db/migrate/20190530071606_create_tweets.rb
+++ db/migrate/20190530071606_create_tweets.rb
@@ -2,8 +2,11 @@ class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
       t.string :text
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :tweets, %i[user_id created_at]
   end
 end
