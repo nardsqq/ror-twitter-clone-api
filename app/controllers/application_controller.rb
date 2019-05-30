fa525c1520fbc55bf2d375a4d9739e7ef9dee217
nardsqq <nardparagas@gmail.com>
2019-05-30 14:08:14 +0800

    Removes unnecessary comments in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   # Prepend protect_from_forgery to the before_action chain manually
   protect_from_forgery prepend: true
 
-  # Call the configured parameters
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Allow selected fields to be updated during mass assignment
   protected
 
   def configure_permitted_parameters
